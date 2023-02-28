d8ddb047d33aeea66cf80aa4e991d42c3d8d69b4
Katorligan <katorligan@gmail.com>
2023-02-28 18:33:26 +0100

    feat: Added admin elements when logged in

---
+++ FrontEnd/admin.js
@@ -0,0 +1,17 @@
+const editBanner = document.querySelector("#edit-mode");
+const loginLink = document.querySelector("#login-link");
+const filters = document.querySelector(".filters");
+
+if (window.localStorage.getItem("token")) {
+	editBanner.style.display = "flex";
+
+	loginLink.href = "index.html";
+	loginLink.innerHTML = "<li>logout</li>";
+
+	loginLink.addEventListener("click", () => {
+		window.localStorage.removeItem("userId");
+		window.localStorage.removeItem("token");
+	});
+
+	filters.style.display = "none";
+}
